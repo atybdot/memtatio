11af026f0c095dd4e0622d2e671b60c0c73e1ed8
atybdot <180611249+atybdot@users.noreply.github.com>
2025-06-20 09:50:00 +0530

    feat: basic chat bot

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -8,4 +8,14 @@ app.post("/chat", async (c) => {
   const google = createGoogleGenerativeAI({
     apiKey: c.env.GOOGLE_GENAI_API,
   });
+  const result = streamText({
+    model: google("gemini-2.5-flash"),
+    messages,
+  });
+  c.header("X-Vercel-AI-Data-Stream", "v1");
+  c.header("Content-Type", "text/plain; charset=utf-8");
+  c.header("Content-Encoding", "none");
+  c.header("x-memtatio-ai", memtatio ?? dataset.username);
+  return stream(c, (s) => s.pipe(result.toDataStream()));
+});
 export default app;
