3ed879f0b33f7057624512fdeb4a583c7f20e523
atybdot <180611249+atybdot@users.noreply.github.com>
2025-06-20 14:50:00 +0530

    feat: prepare dataset for system propmt

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,13 +1,38 @@
 import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import * as dataSets from "@memtatio/datasets";
 import { smoothStream, streamText } from "ai";
 import { Hono, type Next } from "hono";
 import { logger } from "hono/logger";
 import { stream } from "hono/streaming";
 const app = new Hono<{ Bindings: CloudflareBindings }>()
+app.use(logger(),);
 app.post("/chat", async (c) => {
+  const { memtatio, messages } = await c.req.json();
+  console.info("GOT: ", memtatio);
+  const toMimic = dataSets.datasets.find((ai) => ai.username === memtatio);
+  const dataset = toMimic?.username ? toMimic : dataSets.hitesh;
+  toMimic?.username
+    ? null
+    : console.error(
+        "Did not received username\n defaulting to ",
+        dataSets.hitesh.username
+      );
+
+  console.log("[AI]: ", dataset.username);
   const google = createGoogleGenerativeAI({
     apiKey: c.env.GOOGLE_GENAI_API,
   });
+  //prepare string for dataset
+  const examplesString = dataset.data
+    .map(
+      (i) =>
+        `#example\n${
+          i.title.startsWith("where he is talking about")
+            ? "this is title of the video"
+            : "this is title of the video where he is talking about"
+        } "${i.title}" :\n${i.text}\n`
+    )
+    .join("\n");
   const result = streamText({
     model: google("gemini-2.5-flash"),
     messages,
